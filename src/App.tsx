182f8b05cbc333618133bfa7af840df5ae15cf2c
Oktay Nematov <oktayoki123@gmail.com>
2023-04-14 10:36:36 +0400

    deleted react-beautiful-dnd due to its complexity and deprecated support (completely)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 
-import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 import CalculatorConstructor from './components/CalculatorConstructor'
 import WaySwitcher from './components/WaySwitcher'
 import Workspace from './components/Workspace'
 import styles from './App.module.scss'
-import { StrictModeDroppableHelper as Droppable } from './helpers/components/StrictModeDroppableHelper'
 
 function App() {
-
-  const handleDragDrop = (result: DropResult) => {
-    console.log(result)
-  }
 
   return (
     <main className={styles.App}>
